b7fb4f7b25ae3e4663ac7c1b0e613201ef71179e
NewplayerKOR <149351553+NewplayerKOR@users.noreply.github.com>
2025-08-04 22:20:34 +0900

    [feat]: 예시 주소

--- next.config.ts
+++ next.config.ts
@@ -20,8 +20,8 @@ const nextConfig: NextConfig = {
   },
   env: {
     NEXT_PUBLIC_BACKEND_URL: "http://localhost:8080",
-    // NEXT_PUBLIC_BACKEND_URL: "https://your-production-backend.com", 
     NEXT_PUBLIC_WEBSOCKET_URL: "http://localhost:8080/chat",
+    // NEXT_PUBLIC_BACKEND_URL: "https://your-production-backend.com", 
     // NEXT_PUBLIC_WEBSOCKET_URL: "wss://your-production-backend.com/chat",
   },
   // WebSocket 연결을 위한 설정
